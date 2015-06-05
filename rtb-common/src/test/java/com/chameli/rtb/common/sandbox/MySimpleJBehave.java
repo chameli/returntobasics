4ae673d2ae10cef64446263e1c8ec5266af389cb
Anders Lundberg <dan.anders.lundberg@server.fake>
2015-06-05 17:56:52 +0200

    Added support for JBehave and a simple test

---
+++ rtb-common/src/test/java/com/chameli/rtb/common/sandbox/MySimpleJBehave.java
@@ -0,0 +1,37 @@
+package com.chameli.rtb.common.sandbox;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.jbehave.core.configuration.Configuration;
+import org.jbehave.core.configuration.MostUsefulConfiguration;
+import org.jbehave.core.junit.JUnitStories;
+import org.jbehave.core.steps.InjectableStepsFactory;
+import org.jbehave.core.steps.InstanceStepsFactory;
+import org.junit.runner.RunWith;
+
+import de.codecentric.jbehave.junit.monitoring.JUnitReportingRunner;
+
+@RunWith(JUnitReportingRunner.class)
+public class MySimpleJBehave extends JUnitStories {
+
+    public MySimpleJBehave() {
+        JUnitReportingRunner.recommendedControls(configuredEmbedder());
+    }
+
+    @Override
+    public InjectableStepsFactory stepsFactory() {
+        return new InstanceStepsFactory(configuration(), new MySquareSteps());
+    }
+
+    @Override
+    public Configuration configuration() {
+        return new MostUsefulConfiguration();
+    }
+
+    @Override
+    protected List<String> storyPaths() {
+        return Arrays.asList("mysquare.story");
+    }
+
+}
