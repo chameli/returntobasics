53fec7a73494262650a9ecb793414c646cc52059
Anders Lundberg <dan.anders.lundberg@server.fake>
2017-05-24 17:20:39 +0200

    Disk drive letter neutral paths to binaries

--- local-tools/project-bash.bat
+++ local-tools/project-bash.bat
@@ -1,11 +1,11 @@
 @echo off
 
-set JAVA_HOME=C:\Progra~1\Java\jdk1.8.0_73
-set MAVEN_HOME=D:\java\apache-maven-3.3.9
-set ANT_HOME=D:\java\apache-ant-1.9.6
+set JAVA_HOME=\Program\jdk8
+set MAVEN_HOME=\Program\maven3
+set ANT_HOME=\Program\ant1.9
 
 SET CURRENTDIR="%~dp0"
-set BASHHOME=C:\Progra~1\Git
+set BASHHOME=\Program\git2
 
 set PATH=%JAVA_HOME%\bin;%MAVEN_HOME%\bin;%ANT_HOME%\bin;%PATH%
 
